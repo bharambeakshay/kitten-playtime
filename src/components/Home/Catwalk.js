787d6c2338f7a7c24932541025375a69c7920d61
Akshay <bharambeakshay1997@gmail.com>
2021-03-20 00:00:33 -0700

    [Akshay] Refactor . home component updataed with score

--- src/components/Home/Catwalk.js
+++ src/components/Home/Catwalk.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Cat from "../../images/cat.gif";
-import './home.css'
+import "./home.css";
 export default function catwalk(props) {
   return [...Array(props.count)].map((elementInArray, index) => (
     <img
       className=""
       src={Cat}
-      alt='cat'
-      style={{ width: "100px", height: "100px" }}
+      alt="cat"
+      style={{ width: "100px", height: "100px"   }}
     />
   ));
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,28 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import './home.css'
-import Carpet from "../../images/carpet.png"
-import KittenGame from "../../images/kitten game.png"
-import Speaker from "../../images/speaker.svg"
-import Logout from "../../images/logout.svg"
-import background from "../../images/wall.jpg"
-import CatBall from './CatBall'
-import Catwalk from './Catwalk'
+import React from "react";
+import "./home.css";
+import Carpet from "../../images/carpet.png";
+import Speaker from "../../images/speaker.svg";
+import Logout from "../../images/logout.svg";
+import background from "../../images/wall.jpg";
+import CatBall from "./CatBall";
+import Catwalk from "./Catwalk";
 
-// merge 
+// merge
 import "../../style.css";
 import questionAPI from "../question/index";
 import QuestionBox from "../QuestionBox";
 import Result from "../ResultBox";
 
-
-
-const l = "catBall"
-// const randomcatball = Math.floor((Math.random() * 3) + 1);
-// const randomcatwalk = Math.floor((Math.random() * 6) + 1);
-
+const l = "catBall";
+const randomcatball = Math.floor(Math.random() * 3 + 1);
+const randomcatwalk = Math.floor(Math.random() * 6 + 1);
+const numberArrays = [
+    "ZERO",
+    "ONE",
+    "TWO",
+    "THREE",
+    "FOUR",
+    "FIVE",
+    "SIX",
+    "SEVEN",
+    "EIGHT",
+    "NINE",
+];
 
 class Home extends React.Component {
-
     constructor() {
         super();
         this.state = {
@@ -30,8 +37,10 @@ class Home extends React.Component {
             score: 0,
             ans: true,
             responses: 0,
-            randomcatball: Math.floor((Math.random() * 3) + 1),
-            randomcatwalk: Math.floor((Math.random() * 6) + 1)
+            randomcatball: randomcatball,
+            randomcatwalk: randomcatwalk,
+            question: `${numberArrays[randomcatball]} kittens are playing and ${numberArrays[randomcatwalk]} more joins`,
+            answer: randomcatball + randomcatwalk,
         };
     }
 
@@ -43,25 +52,30 @@ class Home extends React.Component {
         });
     };
 
-
-
     componentDidMount() {
         this.getQuestions();
     }
 
     playAgain = () => {
+        let rand1 = Math.floor(Math.random() * 3 + 1);
+        let rand2 = Math.floor(Math.random() * 6 + 1);
         this.getQuestions();
         this.setState({
             responses: 0,
+            randomcatball: rand1,
+            randomcatwalk: rand2,
+            question: `${numberArrays[rand1]} kittens are playing and ${numberArrays[rand2]} more joins`,
+            answer: rand1 + rand2,
         });
     };
 
     computeAnswer = (answer, correct) => {
         if (answer === correct) {
             this.setState({
+                ...this.state,
                 score: this.state.score + 1,
-
             });
+            console.log("score", this.state.score);
         }
 
         this.setState({
@@ -70,48 +84,67 @@ class Home extends React.Component {
     };
 
     correctAnswer = (answer, correct) => {
+        let rand1 = Math.floor(Math.random() * 3 + 1);
+        let rand2 = Math.floor(Math.random() * 6 + 1);
+
         if (answer === correct) {
             this.setState({
                 ans: this.state.ans,
-                randomcatball: Math.floor((Math.random() * 3) + 1),
-                randomcatwalk: Math.floor((Math.random() * 6) + 1)
+
+                randomcatball: rand1,
+                randomcatwalk: rand2,
+
+                question: `${numberArrays[rand1]} kittens are playing and ${numberArrays[rand2]} more joins`,
             });
-        }
-        else {
+        } else {
             this.setState({
                 ans: !this.state.ans,
-                randomcatball: Math.floor((Math.random() * 3) + 1),
-                randomcatwalk: Math.floor((Math.random() * 6) + 1)
+                randomcatball: rand1,
+                randomcatwalk: rand2,
+                question: `${numberArrays[rand1]} kittens are playing and ${numberArrays[rand2]} more joins`,
             });
         }
     };
+    checkAnswers = (answer) => {
+        this.setState({
+            ...this.state,
+            ans: this.state.answer == answer,
+            responses: 1,
+            score: this.state.score + 1,
 
-
-
+        });
+    };
 
     render() {
         return (
             <React.Fragment>
                 <div className="card container">
                     <div className="card-body kitten-card-body">
-                        <div className="room  row" >
+                        <div className="room  row">
                             <img className="room1 col-12" src={background} alt="back" />
 
                             <div className="carpet-div">
                                 <img className="carpet" src={Carpet} alt="carpet" />
                             </div>
-                            <div className="mainContent" >
+                            <div className="mainContent">
                                 {this.state.questionBank.length > 0 &&
                                     this.state.responses < 5 &&
                                     this.state.questionBank.map(
                                         ({ question, answers, correct, questionId }) => (
                                             <QuestionBox
-                                                question={question}
+                                                randomOne={this.state.randomcatball}
+                                                randomTwo={this.state.randomcatwalk}
+                                                question={this.state.question}
                                                 options={answers}
                                                 key={questionId}
-                                                selected={(answer) => this.computeAnswer(answer, correct)}
-                                                corrected={(answer) => this.correctAnswer(answer, correct)}
+                                                selected={(answer) =>
+                                                    this.computeAnswer(answer, correct)
+                                                }
+                                                corrected={(answer) =>
+                                                    this.correctAnswer(answer, correct)
+                                                }
                                                 playAgain={this.playAgain}
+                                                checkAnswers={this.checkAnswers}
                                             />
                                         )
                                     )}
@@ -123,10 +156,18 @@ class Home extends React.Component {
                             <img src={KittenGame} alt="game logo" style={{ height: "150px", width: "150px" }} />
                         </div> */}
                             </div>
-                            <button className="speaker"><img src={Speaker} style={{ width: "20px", height: "20px" }} /></button>
-                            <button className="exit"><img src={Logout} style={{ width: "20px", height: "20px" }} /></button>
+                            <button className="speaker">
+                                <img src={Speaker} style={{ width: "20px", height: "20px" }} />
+                            </button>
+                            <button className="exit">
+                                <img src={Logout} style={{ width: "20px", height: "20px" }} />
+                            </button>
                             <div className="stars">
-                                <div><h2 className="score-board-home">Your Score: {this.state.score.toString()}</h2></div>
+                                <div>
+                                    <h2 className="score-board-home">
+                                        Your Score: {this.state.score.toString()}
+                                    </h2>
+                                </div>
                             </div>
                             <div className="flex1">
                                 <CatBall count={this.state.randomcatball} />
@@ -137,10 +178,9 @@ class Home extends React.Component {
                         </div>
                     </div>
                 </div>
-            </React.Fragment >
-        )
+            </React.Fragment>
+        );
     }
-
 }
 
 export default Home;
